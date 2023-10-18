c7bbaaeaba3cfd8e012ba64081ad1b642fed1b13
Priyanshu-Nandi <anishkumar3787230@gmail.com>
2023-10-18 19:02:29 +0530

    first commit

--- index.js
+++ index.js
@@ -52,6 +52,9 @@ app.post('/messageMe', async(req, res) => {
 }
 
 })
+app.get('/', async(req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
