03f8afb0778cc115e6f1b22cc21733ef58bcec5b
Priyanshu-Nandi <anishkumar3787230@gmail.com>
2023-10-18 18:52:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var cors = require('cors')
+app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('                                      Aishwarya jii miss U')
+let bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+
+const port = 5000
+const mongoose = require('mongoose'); 
+mongoose.set("strictQuery", false);
+const url="mongodb+srv://portfilio:porftfilio123@cluster0.untqikx.mongodb.net/portfilioMessage?retryWrites=true&w=majority"
+const mongooseConnected=mongoose.connect(url);
+ 
+const MessageSchema=mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    message:{
+        type:String,
+        require:true
+    }
+})
+let MessageModel=mongoose.model('MessagePortfilio',MessageSchema)
+
+app.post('/messageMe', async(req, res) => {
+  console.log("sending message")
+  try{
+    const Data=await MessageModel.create({
+        name:req.body.name,
+        email:req.body.email,
+        message:req.body.message
+    })
+   await Data.save();
+    res.status(200).json({
+        status:true,
+        data:Data
+    })
+}catch(e){
+    res.status(404).json({
+        status:false,
+        message:e.message,
+        data:{}
+    })
+}
+
 })
 
 app.listen(port, () => {
